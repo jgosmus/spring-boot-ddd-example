9d0818e006afebc5975a22394277d1a8d3c71d14
Francisco <jgosmus@gmail.com>
2023-02-03 12:13:17 +0100

    Init project

---
+++ src/main/java/com/facespedes/spring_boot_ddd_example/Application.java
@@ -0,0 +1,12 @@
+package com.facespedes.spring_boot_ddd_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
