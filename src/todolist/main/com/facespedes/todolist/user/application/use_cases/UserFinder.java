1588f3ce59657b494f6e60d496bfcf54f134d93b
Francisco <jgosmus@gmail.com>
2023-02-13 12:18:27 +0100

    Refactor

--- src/todolist/main/com/facespedes/todolist/user/application/use_cases/UserFinder.java
+++ src/todolist/main/com/facespedes/todolist/user/application/use_cases/UserFinder.java
@@ -1,20 +1,13 @@
 package com.facespedes.todolist.user.application.use_cases;
 
 import com.facespedes.todolist.shared.domain.Service;
-import com.facespedes.todolist.task.domain.aggregate.Task;
-import com.facespedes.todolist.task.domain.ports.TaskRepository;
-import com.facespedes.todolist.task.domain.services.DomainTaskFinder;
-import com.facespedes.todolist.user.application.dto.TaskResponse;
-import com.facespedes.todolist.user.application.dto.UserFinderResponse;
-import com.facespedes.todolist.user.application.dto.UserTasksFinderResponse;
-import com.facespedes.todolist.user.domain.exceptions.UserNotFoundException;
-import com.facespedes.todolist.user.domain.services.DomainUserFinder;
-import com.facespedes.todolist.user.domain.aggregate.User;
 import com.facespedes.todolist.shared.domain.UserId;
+import com.facespedes.todolist.user.application.dto.UserFinderResponse;
+import com.facespedes.todolist.user.domain.aggregate.User;
+import com.facespedes.todolist.user.domain.exceptions.UserNotFoundException;
 import com.facespedes.todolist.user.domain.ports.UserRepository;
+import com.facespedes.todolist.user.domain.services.DomainUserFinder;
 import lombok.AllArgsConstructor;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Service
